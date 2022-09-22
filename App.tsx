ce6d8e6711abdca368d2cde6a4b230b84419d3e9
jh.terencio@gmail.com <94974380+Jh-Terencio@users.noreply.github.com>
2022-09-21 21:23:46 -0300

    src adicionado

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';//Biblioteca de fontes 
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';//Biblioteca que não permite a abertura do app até x condição ser verdadeira linha 19
+
+import { Home } from './src/screens/Home';
+import { Background } from './src/components/Background';
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){ //criamos uma constante para caso as fontes não tenha sido carregadas o app não termine de carregar
+    return <AppLoading/>
+  }
+
+  return(
+    /*Como o return so pode retornar 1 unica coisa 
+    usamos esse <>, chamado de fragment, para retornar
+    mais de uma coisa*/ 
+    <Background>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+    <Home/>
+    </Background>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
